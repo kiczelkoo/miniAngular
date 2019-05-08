548fd4eac33aba60955151c167fdaf362191d662
Olga <olga.pozn@gmail.com>
2019-05-08 23:04:27 +0200

    add angular app

---
+++ miniAngularClient/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mini-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'miniAngularClient';
+}
