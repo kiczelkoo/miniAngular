2fbfbd78a7bff2416ab91ef267b60163921ba2ba
Poznanska <olga.poznanska@capgemini.com>
2019-05-12 20:06:22 +0200

    add working anguular + mvc configuration"

---
+++ src/main/java/com/craftsmanship/miniangular/WebMvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.craftsmanship.miniangular;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/miniAngularClient/**")
+                .addResourceLocations("classpath:/miniAngularClient");
+    }
+}
