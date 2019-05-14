48fc230ac2eed1058133622e26f78fc1a4316a81
Poznanska <olga.poznanska@capgemini.com>
2019-05-14 11:23:03 +0200

    adjust mvc controller

--- src/main/java/com/craftsmanship/miniangular/web/SearchController.java
+++ src/main/java/com/craftsmanship/miniangular/web/SearchController.java
@@ -22,13 +22,7 @@ public class SearchController {
 
     @GetMapping("/recipes")
     public ModelAndView getRecipes(@RequestParam(name = "product") final List<String> products) {
-        List<RecipeModel> recipeModels = this.dataProvider.getRecipes()
-                .stream()
-                .filter(recipe -> recipe.getIngredients().stream().map(
-                        IngredientModel::getProductName).anyMatch(products::contains))
-                .collect(Collectors.toList());
         return new ModelAndView("view/search-results")
-                .addObject("recipes", recipeModels)
                 .addObject("selectedProducts", products)
                 .addObject("products", this.dataProvider.getProducts());
 
